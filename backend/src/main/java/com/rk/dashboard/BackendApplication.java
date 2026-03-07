0bd9a490e378236ebdde40ac2fb17fda68617bc1
talarivs <talari.vs@simadvisorytech.com>
2026-03-07 20:31:02 +0530

    updated

--- backend/src/main/java/com/rk/dashboard/BackendApplication.java
+++ backend/src/main/java/com/rk/dashboard/BackendApplication.java
@@ -26,8 +26,8 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
             }
         };
     }
